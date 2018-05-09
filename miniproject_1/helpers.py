18a3938f6fdffb9bffda9542b37564743b8a95de
Pierre-Antoine Desplaces <padesplaces@gmail.com>
2018-05-09 12:11:02 +0200

    git cleanup

--- miniproject_1/helpers.py
+++ miniproject_1/helpers.py
@@ -19,29 +19,28 @@ def compute_accuracy(labels, predictions):
     return 1-error_rate
 
 
-def train_model(model, train_input, train_target, mini_batch_size, nb_epochs=100, learning_rate=1e-3):
+def train_model(model, train_input, train_target, mini_batch_size, nb_epochs=100, learning_rate=1e-2):
+	model.train()
 	criterion = nn.CrossEntropyLoss()
 	optimizer = optim.SGD(model.parameters(), lr = learning_rate)
 	for e in range(0, nb_epochs):
 		sum_loss = 0
 		for b in range(0, train_input.size(0), mini_batch_size):
 			output = model(train_input.narrow(0, b, mini_batch_size))
-			#print(train_input.narrow(0, b, mini_batch_size).size())
-			#print(output.size())
 			loss = criterion(output, train_target.narrow(0, b, mini_batch_size))
 			sum_loss = sum_loss + loss.data[0]
 			model.zero_grad()
 			loss.backward()
 			optimizer.step()
-		print(e, sum_loss)
+		print("Epoch", e, "loss =", sum_loss)
 
 def compute_nb_errors(model, data_input, data_target, mini_batch_size):
 	nb_data_errors = 0
+	model.eval()
 	for b in range(0, data_input.size(0), mini_batch_size):
 		output = model(data_input.narrow(0, b, mini_batch_size))
 		_, predicted_classes = torch.max(output.data, 1)
 		for k in range(0, mini_batch_size):
-			#if data_target.data[b + k, predicted_classes[k]] < 0:
 			if data_target.data[b + k] != predicted_classes[k]:
 				nb_data_errors = nb_data_errors + 1
 	return nb_data_errors
